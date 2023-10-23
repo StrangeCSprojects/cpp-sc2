3b94da41ddaec5ea5fe80185660f334fa74195c7
Clstrange <122171025+StrangeCSprojects@users.noreply.github.com>
2023-10-23 00:17:49 -0600

    finished prototype

--- main.py
+++ main.py
@@ -4,69 +4,152 @@
 # import pytesseract # A library which can help us with retrieving in-game screen info
 # import pyautogui # Some extra GUI tools we could use
 import tkinter as tk
+from tkinter import ttk
 import subprocess
+import psutil
+import threading
 import time
+import os
+import csv
 
 
-# Function to check if StarCraft 2 is running
-def isStarCraftRunning():
-    process_list = subprocess.Popen(["tasklist"], stdout=subprocess.PIPE, text=True)
-    output = process_list.communicate()[0]
-    return any("SC2" in line or "StarCraft" in line for line in output.splitlines())
+# # Function to check if StarCraft 2 is running
+# def isStarCraftRunning():
+#     process_list = subprocess.Popen(["tasklist"], stdout=subprocess.PIPE, text=True)
+#     output = process_list.communicate()[0]
+#     return any("SC2" in line or "StarCraft" in line for line in output.splitlines())
 
+class BuildOrderOverlay(tk.Tk):
 
-# Create a function to update the GUI
-def updateGui(window, canvas):
-    if isStarCraftRunning():
-        canvas.itemconfig("status_message", text="StarCraft II is running!")
-        window.after(100, updateGui, window, canvas)  # Check every 1/2 second
-    else:
-        canvas.itemconfig("status_message", text="Closing StarCraft II...")
-        window.after(3000, window.destroy)
-        print("Your StarCraft II session has ended.")
+    def __init__(self):
+        super().__init__()
 
+        # Set the window title
+        self.title("SC2 Build Order Overlay")
 
-# Main entry code
-def main():
-    # Print to console that overlay has begun
-    print("StarCraft II Overlay has started.")
+        # Set the window to be always on top
+        self.attributes("-topmost", True)
+       
+        # Set window transparency (0.0: fully transparent, 1.0: fully opaque)
+        self.attributes('-alpha', 0.9)
+
+        # Create the table headers
+        columns = ("Time", "Unit/Building")
+        self.tree = ttk.Treeview(self, columns=columns, show="headings")
+
+        # Define the column headings and widths
+        self.tree.heading("Time", text="Time")
+        self.tree.heading("Unit/Building", text="Unit/Building")
+
+        self.tree.column("Time", width=60)
+        self.tree.column("Unit/Building", width=150)
+
+        # Insert data
+        build_order = [
+            ("0:17", "Supply Depot"),
+            ("0:39", "Barracks"),
+            ("0:43", "Refinery"),
+            ("1:27", "Marine"),
+            ("1:42", "Command Center"),
+            ("1:57", "Supply Depot"),
+            ("2:20", "Barracks"),
+            ("2:20", "Factory"),
+            
+            # ... add more as needed
+        ]
+
+        for item in build_order:
+            self.tree.insert("", "end", values=item)
+
+        self.tree.pack(pady=20)
+
+class LiveBuildOrderOverlay(tk.Tk):
+    def __init__(self):
+
+        super().__init__()
+
+        # Set the window title
+        self.title("SC2 Build Order Overlay")
+
+        # Set the window to be always on top
+        self.attributes("-topmost", True)
+       
+        # Set window transparency (0.0: fully transparent, 1.0: fully opaque)
+        self.attributes('-alpha', 0.9)
+
+        # Create the table headers
+        columns = ("Time", "Unit/Building")
+        self.tree = ttk.Treeview(self, columns=columns, show="headings")
+
+        # Define the column headings and widths
+        self.tree.heading("Time", text="Time")
+        self.tree.heading("Unit/Building", text="Unit/Building")
+
+        self.tree.column("Time", width=60)
+        self.tree.column("Unit/Building", width=150)
+
+        # Insert data
+        build_order = [
+            # ... add more as needed
+        ]
+
+        for item in build_order:
+            self.tree.insert("", "end", values=item)
+
+        self.tree.pack(pady=20)
+
+        self.csv_filepath = "build/bin/output.csv"
+        self.last_check_time = os.path.getmtime(self.csv_filepath)
+        self.poll_csv()
+
+    def has_file_changed(self, filepath, last_check_time):
+        """
+        Check if the file at 'filepath' has been modified since 'last_check_time'.
+        Returns (has_changed, new_check_time).
+        """
+        current_time = os.path.getmtime(filepath)
+        return current_time > last_check_time, current_time
     
-    # Create the main window
-    window = tk.Tk()
-    window.title("StarCraft 2 Overlay")
+    def update_table_from_csv(self, csv_filepath):
+        with open(csv_filepath, "r") as file:
+            rows = list(csv.reader(file))
 
-    # Create a black canvas
-    canvas = tk.Canvas(window, width=400, height=100, bg="black")
-    canvas.pack()
+        # Assuming first row is headers
+        headers = rows[0]
+        for col, header in enumerate(headers):
+            self.tree.heading(col, text=header)
 
-    # Add the message to the canvas
-    message = canvas.create_text(
-        200,
-        50,
-        text="Started StarCraft 2",
-        fill="white",
-        font=("Helvetica", 16),
-        anchor="center",
-        tags="status_message"  # Use the correct tag
-    )
+        # Remove old items and add new items
+        for row in self.tree.get_children():
+            self.tree.delete(row)
 
-    # Center the window on the screen
-    window.update_idletasks()
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
-    window_width = window.winfo_width()
-    window_height = window.winfo_height()
-    x = (screen_width - window_width) // 2
-    y = (screen_height - window_height) // 2
-    window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+        for row_data in rows[1:]:
+            self.tree.insert("", "end", values=row_data)
+    def poll_csv(self):
+        changed, new_time = self.has_file_changed(self.csv_filepath, self.last_check_time)
+        if changed:
+            self.update_table_from_csv(self.csv_filepath)
+            self.last_check_time = new_time
 
-    # Start checking if StarCraft 2 is running
-    updateGui(window, canvas)
+        # Poll every 5 seconds (5000 milliseconds)
+        self.after(5000, self.poll_csv)
+    
+def check_for_sc2():
+    while True:
+        
+        process_names = [proc.name().lower() for proc in psutil.process_iter(attrs=['name'])]
 
-    # Run the GUI main loop
-    window.mainloop()
+        print("Checking for sc2 process...")
 
+        if "sc2_x64.exe" in process_names:
+            print("Process found!")
+            app = BuildOrderOverlay()
+            app2 = LiveBuildOrderOverlay()
+            app.mainloop()
+            app2.mainloop()
+            break  # End this function once overlay is launched
 
-# Run the program
+        time.sleep(1)  # Wait every 5 seconds before checking again
+
 if __name__ == "__main__":
-    main()
+    check_for_sc2()
