cac6ead277625f79b568acb540c3c53d77974faf
Clstrange <122171025+StrangeCSprojects@users.noreply.github.com>
2024-01-28 22:42:00 -0700

    determine build order

---
+++ BuildMaster/README.txt
@@ -0,0 +1,7 @@
+--instructions--
+1. Make sure you have installed sc2reader
+2. Update line 6 with local paths to the replays folder
+
+
+--commands--
+pip install sc2reader
--- BuildMaster/main.py
+++ BuildMaster/main.py
@@ -2,13 +2,14 @@ import os
 import sc2reader
 from enum import Enum
 
-# Specify the directory path
-folder_path = "B:/cpp-sc2/BuildMaster/Replays"
+# Build order types
+class BuildOrder(Enum):
+    UNKNOWN = 0
+    AGRESSION = 1
+    STANDARD = 2
+    ECONOMY = 3
 
-# Starcraft 2 username
-player_name = "Cstrange"
-
-def race_winrate(player_name):
+def replay_analysis(player_name, folder_path):
     """Gather data to determine winrate based on race"""
     replay_counter = 0
 
@@ -33,8 +34,8 @@ def race_winrate(player_name):
             player_two_name = player_two.name
 
             # Player races
-            player_one_race = player_one.race 
-            player_two_race = player_two.race
+            player_one_race = player_one.play_race 
+            player_two_race = player_two.play_race
 
             # Game winner name
             if replay.winner:
@@ -42,52 +43,63 @@ def race_winrate(player_name):
                     if player.result == 'Win':
                         winner_name = player.name
 
+            # Determine build order            
+            print(f"Player Name: {player_name}\nGame Mode: {game_mode}\nPlayer One: {player_one_name} - {player_one_race}\nPlayer Two: {player_two_name} - {player_two_race}\nWinner: {winner_name}")
+            build_order(replay)
+            print("\n")
 
-"""Determine Build Type"""
-class BuildOrder(Enum):
-    UNKNOWN = 0
-    AGRESSION = 1
-    STANDARD = 2
-    ECONOMY = 3
+def build_order(replay):
+    """Determine build order"""
+    command_center_count = 1
+    production_building_count = 0
+    build_order = BuildOrder.UNKNOWN
+
+    # Initialize a dictionary to hold events for each player
+    player_events = {player: [] for player in replay.players}
+
+    # Iterate through events and process only unit events
+    for event in replay.events:
+        if isinstance(event, sc2reader.events.UnitInitEvent):
+            player = event.unit.owner
+            if player:
+                player_events[player].append(event)
+
+    for player, events in player_events.items():
+        if player.name == "Cstrange":
+            print(f"Events for Player {player.name}:")
+            for event in events:
+
+                game_speed_factor = 1.4
+                real_time_seconds = int(event.second / game_speed_factor)
+                # print(f" - {event.unit.name} at {int(real_time_seconds // 60)}.{real_time_seconds % 60}s")
+                if event.unit.name == "OrbitalCommand" or event.unit.name == "OrbitalCommandFlying" :
+                    command_center_count += 1
+                elif event.unit.name == "Barracks":
+                    production_building_count += 1
+                elif event.unit.name == "Factory":
+                    production_building_count += 1
+                elif event.unit.name == "Starport":
+                    production_building_count += 1
+                
+                if event.second < 300:
+                    if command_center_count >= 3:
+                        build_order = BuildOrder.ECONOMY
+                    elif production_building_count >= 7:
+                        build_order = BuildOrder.AGRESSION
+                    elif command_center_count == 2 and production_building_count < 7:
+                        build_order = BuildOrder.STANDARD
+    # print(command_center_count)
+    # print(production_building_count)
+    print(f"Build Order: {build_order.name}")
 
 
-command_center_count = 1
-production_building_count = 0
-build_order = BuildOrder.UNKNOWN
+def main():
+    # Specify the directory path
+    folder_path = "C:/cpp-sc2/BuildMaster/Replays" # REMEBER TO UPDATE!!!
 
-replay = sc2reader.load_replay("B:\cpp-sc2\BuildMaster\Replays\Solaris LE (17).SC2Replay", load_map=True)
-
-# Initialize a dictionary to hold events for each player
-player_events = {player: [] for player in replay.players}
-
-# Iterate through events and process only unit events
-for event in replay.events:
-    if isinstance(event, sc2reader.events.UnitInitEvent):
-        player = event.unit.owner
-        if player:
-            player_events[player].append(event)
-
-for player, events in player_events.items():
-    if player.name == "Cstrange":
-        print(f"Events for Player {player.name}:")
-        for event in events:
-            print(f" - {event.unit.name} at {event.second // 60}.{event.second % 60}s")
-            if event.unit.name == "OrbitalCommand":
-                command_center_count += 1
-            elif event.unit.name == "Barracks":
-                production_building_count += 1
-            elif event.unit.name == "Factory":
-                production_building_count += 1
-            elif event.unit.name == "Starport":
-                production_building_count += 1
-            
-            if event.second < 300:
-                if command_center_count >= 3:
-                    build_order = BuildOrder.ECONOMY
-                elif production_building_count >= 7:
-                    build_order = BuildOrder.AGRESSION
-                elif command_center_count == 2 and production_building_count < 7:
-                    build_order = BuildOrder.STANDARD
-print(command_center_count)
-print(production_building_count)
-print(build_order.name)
+    # Starcraft 2 username
+    player_name = "Cstrange"
+    
+    replay_analysis(player_name,folder_path)
+if __name__ == "__main__":
+    main()
